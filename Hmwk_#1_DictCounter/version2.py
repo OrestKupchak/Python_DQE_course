1a5984fc19902fa8ae268a74d870af1753685629
Orest <orest.kupchak@gmail.com>
2019-12-10 00:38:55 +0200

    Version 1.2
    - added export into resilt file
    - fixed exceptions

--- Hmwk_#1_DictCounter/version2.py
+++ Hmwk_#1_DictCounter/version2.py
@@ -3,16 +3,17 @@ import re
 from collections import Counter, OrderedDict
 from os import path
 
-class FileFormatException(Exception):
+
+class FileException(Exception):
 
     def __init__(self, datasource):
-        #self.datasource = datasource
+       
         if not path.exists(datasource):
-            raise FileNotFoundError("File '" + datasource + "' doesn't exist under the defined path!")
-        #elif not datasource.lower().endswith('.txt'):
-        #   print('Wrong file format, file is not ".txt" !')
+            Exception.__init__(self, "File '" + datasource + "' doesn't exist under the defined path!")
+        elif not datasource.lower().endswith('.txt'):
+            Exception.__init__(self, 'Wrong file format, file is not ".txt" !')
         elif open(datasource, "r").getsize() == 0:
-            print('File is empty!')
+            Exception.__init__(self,'File is empty!')
 
             
 
@@ -27,14 +28,26 @@ class FrequencyCheck():
 
         if path.exists(self.textfile):
             if not self.textfile.lower().endswith('.txt'):
-                raise FileFormatException(self.textfile)
+                raise FileException(self.textfile)
             else:
                 self.datasource = open(self.textfile, "r")
         else:
-            raise FileFormatException(self.textfile)
+            raise FileException(self.textfile)
+
+    #Put result into resultfile
+    def get_freq_file(self):
+        
+        result_file = "Frequency_check_" + self.textfile + '_' + str(path.getatime(self.textfile)) + ".txt"
+        frequency = open(result_file,"w+")
+        result = self.get_freq()
+        for word in result:
+            frequency.write(str(result[key] + " : " + result[value] + "\n")
+            #print(str(item) + "\n")
+        frequency.close()
 
 
-    #@classmethod
+
+    #Print resuls without putting into result file
     def get_freq(self, pattern = None):
         
         #use default check pattern or add explicitly
@@ -43,7 +56,7 @@ class FrequencyCheck():
         else:
             self.pattern = re.compile(r'' + pattern)
 
-        #get data from gile
+        #get data from file
         text = self.datasource.read()
 
         #if file is not empty check text using defined patter
@@ -51,20 +64,12 @@ class FrequencyCheck():
             words = re.findall(self.pattern, text.lower()) 
         else:
             print('No text found!')
-            raise FileFormatException(self.textfile)
+            raise FileException(self.textfile)
 
         #return result as dictionary {"word": freq_count} sorted alphabetically
         return dict(OrderedDict(sorted(Counter(words).items(), key=lambda t: t[0])))
         
 
-    #Put result into resultfile
-    def get_freq_file(self):
-        
-        result_file = "Frequency_check_" + self.textfile + str(path.getatime()) + ".txt"
-        frequency = open(result_file,"w+")
-        for item in result:
-            frequency.write(str(item) + "\n")
-        frequency.close()
 
 
     # close the file after reading
@@ -75,10 +80,10 @@ class FrequencyCheck():
         
 
 def main():
-    doc = FrequencyCheck("xxx.docx")
-    result = doc.get_freq()
-
-    print(result)
+    doc = FrequencyCheck("dummy.txt")
+    #result = doc.get_freq()
+    doc.get_freq_file()
+    #print(result)
 
 if __name__ == "__main__":
     main()
