969690de88fd0110c7b2a0a456b5142582734e2b
OrestKupchak <orest.kupchak@gmail.com>
2019-12-09 18:04:55 +0200

    Version 1.1:
    - added regexp for apostrophe handling
    - added exception handling

--- Hmwk_#1_DictCounter/dict.py
+++ Hmwk_#1_DictCounter/dict.py
@@ -3,46 +3,64 @@ import re
 from collections import Counter, OrderedDict
 from os import path
 
-class FileFormatException():
+class FileFormatException(Exception):
 
     def __init__(self, datasource):
         #self.datasource = datasource
-        if not datasource.lower().endswith('.txt'):
-            print('Wrong file extension, file is not ".txt" !')
+        if not path.exists(datasource):
+            raise FileNotFoundError("File '" + datasource + "' doesn't exist under the defined path!")
+        #elif not datasource.lower().endswith('.txt'):
+        #   print('Wrong file format, file is not ".txt" !')
         elif open(datasource, "r").getsize() == 0:
             print('File is empty!')
-        else:
-            print('File with given name doesn\'t exist !')
+
+            
 
 
 
 class FrequencyCheck():
-
+    
     #access file needed to be processed
     def __init__(self, textfile):    
-        if path.exists(textfile):
-            if not textfile.lower().endswith('.txt'):
-                raise FileFormatException(textfile)
-            else:
-                self.datasource = open(textfile, "r")
-        else:
-            raise FileFormatException(textfile)
+        
+        self.textfile = textfile
 
+        if path.exists(self.textfile):
+            if not self.textfile.lower().endswith('.txt'):
+                raise FileFormatException(self.textfile)
+            else:
+                self.datasource = open(self.textfile, "r")
+        else:
+            raise FileFormatException(self.textfile)
 
 
     #@classmethod
-    def get_freq(self):
-        text = self.datasource.read()
-        if text is not None:
-            words = re.findall(r'\w+', text.lower())
+    def get_freq(self, pattern = None):
+        
+        #use default check pattern or add explicitly
+        if pattern is None:
+            self.pattern = re.compile(r'([\w\'+]+\w)')
         else:
-            return None
-        return dict(OrderedDict(sorted(Counter(words).items(), key=lambda t: t[0])))
-        #print(result)
+            self.pattern = re.compile(r'' + pattern)
 
+        #get data from gile
+        text = self.datasource.read()
+
+        #if file is not empty check text using defined patter
+        if text is not None:
+            words = re.findall(self.pattern, text.lower()) 
+        else:
+            print('No text found!')
+            raise FileFormatException(self.textfile)
+
+        #return result as dictionary {"word": freq_count} sorted alphabetically
+        return dict(OrderedDict(sorted(Counter(words).items(), key=lambda t: t[0])))
+        
+
+    #Put result into resultfile
     def get_freq_file(self):
-        #Put result into resultfile
-        result_file = "Frequency_check_" + textfile + str(path.getatime()) + ".txt"
+        
+        result_file = "Frequency_check_" + self.textfile + str(path.getatime()) + ".txt"
         frequency = open(result_file,"w+")
         for item in result:
             frequency.write(str(item) + "\n")
@@ -57,9 +75,9 @@ class FrequencyCheck():
         
 
 def main():
-    doc = FrequencyCheck("xxx.txt")
+    doc = FrequencyCheck("xxx.docx")
     result = doc.get_freq()
-    #doc.get_JSON(csvFilePath, target)
+
     print(result)
 
 if __name__ == "__main__":
