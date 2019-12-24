c8a484203825afb5be5b0593fae5334f7b4890f5
OrestKupchak <orest.kupchak@gmail.com>
2019-12-24 17:48:54 +0200

    Version 1.2.2

--- Hmwk_#3_Hangman/reduce.py
+++ Hmwk_#3_Hangman/reduce.py
@@ -2,22 +2,38 @@ import functools
 import random
 from functools import reduce
 
-
+'''
 wordlist = ['zenith', 'zealot', 'yearn', 'yawner', 'xenophobia', 'x-axis', 'wonky', 'wanton', 'vermillion', 'vague', 'unique', 'uncanny'
 , 'tenacious', 'tangible', 'serene', 'saquinavir', 'rhetorical', 'rambunctious', 'quixotic', 'quell', 'pique', 'paradigm'
 , 'oxymoron', 'optimistically', 'nostalgic', 'narrative', 'misanthrope', 'melancholy', 'lucid', 'lethargic', 'ken'
 , 'karma', 'jurisdiction', 'jejune', 'irony', 'integrity', 'hypnosis', 'hyperbole', 'guise', 'gallivant', 'fortitude']
+'''
 
+#wordlist = str(open("words.txt", "r").readlines())
+#print(wordlist)
+wordlist = [word.strip().lower() for word in open("words.txt", "r").readlines()]
+#print(wordlist)
+
+
+
+stripped = ["aaa", "bbb"]
+print(stripped)
 def check_alpha(wordlist):
-    for word in wordlist:
-        if word.isalpha() == True:
-            wordlist.append(word)
+#    for word in wordlist:
+#        #if word.isalpha() == True:
+#        stripped.append(word)
 
-stripped = []
+#print(stripped)
+    #lambda wordlist, word : [print('xxx') if word.isalpha() == True else None for word in wordlist]
+    #functools.reduce(lambda wordlist, word : [stripped.append(word) if word.isalpha() == True else None for word in wordlist], wordlist)
+#functools.reduce(check_alpha(wordlist), wordlist)
 
 
-print(reduce(check_alpha(wordlist), wordlist))
+    return lambda wordlist : [stripped.append(word) if word.isalpha() == True else None for word in wordlist]
+#check = doc(wordlist)
+print(stripped)
 
+check_alpha(wordlist)
 '''
 def mult(words):
     #print("x=",x," y=",y)
